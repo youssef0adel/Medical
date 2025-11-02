6fd7062887c72487568d6d13860c970072b2755b
youssef0adel <yohd6666@gmail.com>
2025-11-02 04:57:35 +0200

    update: تعديل جديد في المشروع

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -11,7 +11,7 @@ const Loader = () => {
   if (!loading) return null;
   return (
     <>
-      <div className="vh-100 d-flex justify-content-center align-items-center position-fixed top-0 bottom-0 end-0 start-0">
+      <div className="loader vh-100 d-flex justify-content-center align-items-center position-fixed top-0 bottom-0 end-0 start-0">
         <video autoPlay loop muted playsInline className="loader">
           <source src={vid1} type="video/mp4" />
         </video>
